55d1533e3556f09ace27e4e0d6fd0233792293a5
Sigmund Frost <sigmafi.tech@gmail.com>
2025-03-05 08:29:52 +0100

    Initial commit

---
+++ app/interfaces/productInterface.ts
@@ -0,0 +1,46 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+  description: string;
+  images: {
+    gallery: Image[];
+  };
+  specifications: {
+    material: string;
+    color: string;
+    size: string;
+    weight: number;
+    height: number;
+    brand: string;
+  };
+  sku: string;
+  shortDescription: string;
+  reviewed: string;
+  reviews: Reviews;
+}
+
+export interface Image {
+  id: number;
+  url: string;
+  alt: string;
+  title: string;
+}
+
+export interface Reviews {
+  rating: number;
+  totalReviews: number;
+  customerReviews: CustomerReview[];
+}
+
+export interface CustomerReview {
+  name: string;
+  email: string;
+  rating: number;
+  review: string;
+}
+
+export interface ProductList {
+  products: Product[];
+}
